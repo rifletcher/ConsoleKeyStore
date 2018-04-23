718d0a8dcb3d288c1d625d021b502823a47a0c69
Robert Fletcher <rifletcher@hotmail.com>
2018-04-23 13:44:10 +0300

    Initial commit

---
+++ ConsoleKeyStore/Program.cs
@@ -0,0 +1,145 @@
+﻿using IdentityModel.OidcClient;
+using Microsoft.Net.Http.Server;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleKeyStore
+{
+    class Program
+    {
+        static string _authority = "http://localhost:8080/auth/realms/test";
+        
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        public static async Task MainAsync()
+        {
+            await SignInAsync();
+
+            Console.ReadKey();
+        }
+
+        private async static Task SignInAsync()
+        {
+            string redirectUri = string.Format("http://127.0.0.1:7890/");
+            var settings = new WebListenerSettings();
+            settings.UrlPrefixes.Add(redirectUri);
+            var http = new WebListener(settings);
+
+            http.Start();
+
+            var options = new OidcClientOptions
+            {
+                Authority = _authority,
+                ClientId = "demo-app",
+                RedirectUri = redirectUri,
+                Scope = "openid",
+                FilterClaims = true,
+                LoadProfile = true,
+                Flow = OidcClientOptions.AuthenticationFlow.AuthorizationCode,
+                Policy = new Policy() { RequireAccessTokenHash = false }
+
+            };
+
+            var client = new OidcClient(options);
+            var state = await client.PrepareLoginAsync();
+
+            OpenBrowser(state.StartUrl);
+
+            var context = await http.AcceptAsync();
+            var formData = GetRequestPostData(context.Request);
+
+            if (formData == null)
+            {
+                Console.WriteLine("Invalid response");
+                return;
+            }
+
+            await SendResponseAsync(context.Response);
+
+            var result = await client.ProcessResponseAsync(formData, state);
+
+            ShowResult(result);
+        }
+
+        public static void OpenBrowser(string url)
+        {
+            try
+            {
+                Process.Start(url);
+            }
+            catch
+            {
+                // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        public static string GetRequestPostData(Request request)
+        {
+            if (!request.HasEntityBody)
+            {
+                return null;
+            }
+
+            using (var body = request.Body)
+            {
+                using (var reader = new StreamReader(body))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        private static async Task SendResponseAsync(Response response)
+        {
+            var responseString = $"<html><head></head><body>Please return to the app.</body></html>";
+            var buffer = Encoding.UTF8.GetBytes(responseString);
+
+            response.ContentLength = buffer.Length;
+
+            var responseOutput = response.Body;
+            await responseOutput.WriteAsync(buffer, 0, buffer.Length);
+            responseOutput.Flush();
+        }
+
+        private static void ShowResult(LoginResult result)
+        {
+            if (result.IsError)
+            {
+                Console.WriteLine("\n\nError:\n{0}", result.Error);
+                return;
+            }
+
+            Console.WriteLine("\n\nClaims:");
+            foreach (var claim in result.User.Claims)
+            {
+                Console.WriteLine("{0}: {1}", claim.Type, claim.Value);
+            }
+
+            Console.WriteLine($"\nidentity token: {result.IdentityToken}");
+            Console.WriteLine($"access token:   {result.AccessToken}");
+            Console.WriteLine($"refresh token:  {result?.RefreshToken ?? "none"}");
+        }
+
+    }
+}
